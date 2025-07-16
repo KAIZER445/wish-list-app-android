8c79f519784dc0d208795d8ab384c642e0fc3459
KAIZER445 <prashanttamang861@gmail.com>
2025-07-16 15:15:27 +0545

    added new icon for navigation and added FAB scaffold

--- app/src/main/java/com/example/wishlistapp/TopBar.kt
+++ app/src/main/java/com/example/wishlistapp/TopBar.kt
@@ -1,11 +1,18 @@
 package com.example.wishlistapp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +21,22 @@ fun AppBarView(
     title: String,
     onBackedNavClicked:()->Unit = {},
 ){
+    val navigationIcon: (@Composable () -> Unit)? =
+        if (!title.contains("Wish List") ){
+            {
+            IconButton(onClick = { onBackedNavClicked() }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = null,
+                )
+            }
+            }
+        }else
+        {
+           null
+        }
+
     TopAppBar(title = { Text(text = title, color = colorResource(id = R.color.white),
         modifier = Modifier
             .padding(start = 4.dp)
@@ -21,5 +44,6 @@ fun AppBarView(
     },
     elevation = 3.dp,
     backgroundColor = colorResource(id = R.color.app_bar_color),
+    navigationIcon = navigationIcon
     )
 }
