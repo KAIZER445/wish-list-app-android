2d3cbd07dc3dfda2b48d2a6ee67923349b9364d0
KAIZER445 <prashanttamang861@gmail.com>
2025-07-18 17:43:41 +0545

    navigation setup

---
+++ app/src/main/java/com/example/wishlistapp/Navigation.kt
@@ -0,0 +1,21 @@
+package com.example.wishlistapp
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(viewModel: ViewModel = viewModel(), navController: NavHostController = rememberNavController()){
+    NavHost(navController = navController,
+        startDestination = Screen.HomeScreen.route)
+    {
+        composable(Screen.HomeScreen.route){
+            HomeView()
+        }
+    }
+}
