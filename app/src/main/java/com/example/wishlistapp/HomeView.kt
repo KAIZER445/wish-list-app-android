c9931e4a774d7691f08b017d50a050bf0a8a8df4
KAIZER445 <prashanttamang861@gmail.com>
2025-07-26 19:59:15 +0545

    application complete

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -1,15 +1,20 @@
 package com.example.wishlistapp
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -68,10 +73,24 @@ fun HomeView(
 
                 SwipeToDismiss(
                     state = dismissState,
-                    background = {},
+                    background = {
+                          val color by animateColorAsState(
+                              if (dismissState.dismissDirection == DismissDirection.EndToStart) Color.Red else Color.Transparent
+                              , label = ""
+                          )
+                        val alignment = Alignment.CenterEnd
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .background(color)
+                                .padding(horizontal = 20.dp),
+                            contentAlignment = alignment
+                        ){
+                            Icon(Icons.Default.Delete, contentDescription = null, tint = Color.White)
+                        }
+                    },
                     directions = setOf(
-                        DismissDirection.EndToStart,
-                        DismissDirection.StartToEnd
+                        DismissDirection.EndToStart
                     ),
                     dismissContent = {
                         WishItem(wish = wish) {
